3314870b923e66a4f5f0d57e54b3685ff0694e62
shanta_islam <shanta@etlimited.net>
2024-06-01 10:55:28 +0600

    order module import in the app.ts from the order route

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express, { Request, Response } from "express";
 import { ProductRoutes } from "./app/modules/product/product.route";
 import { errorHandler, notFoundHandler } from "./middlewares/errorHandler";
+import { OrderRoutes } from "./app/modules/order/order.route";
 const app = express();
 
 //parsers
